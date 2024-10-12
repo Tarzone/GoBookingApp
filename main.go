ae137c1bcd6dc9a38e74ccd83d62e09726a72f07
Tarzone <walekunle@gmail.com>
2024-10-12 15:55:45 -0600

    Initialize GoLang Project

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+
+	fmt.Println("Welcome to", conferenceName, "Hello World")
+	fmt.Println("Get yout tickets here to attend")
+
+}
