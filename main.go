4c48e986af470e028825af490b1ccac2c31d7525
Tarzone <walekunle@gmail.com>
2024-10-14 18:01:51 -0600

    conditionals and validity

--- main.go
+++ main.go
@@ -53,23 +53,52 @@ func main() {
 		// fmt.Printf("User %v booked %v tickets.\n", firstName, userTickets)
 		// bookings[49] = firstName + " " + lastName
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		// fmt.Printf("The whole slice: %v\n", bookings)
-		// fmt.Printf("The whole array: %v\n", bookings[0])
-		// fmt.Printf("Slice type: %v\n", bookings)
-		// fmt.Printf("Slice length: %v\n", len(bookings))
+		// isValidCity := city == "Singapore" || city == "London"
+		// !isValidCity
+		// isInvalidCity := city != "Singapore" || city != "London"
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			// fmt.Printf("The whole slice: %v\n", bookings)
+			// fmt.Printf("The whole array: %v\n", bookings[0])
+			// fmt.Printf("Slice type: %v\n", bookings)
+			// fmt.Printf("Slice length: %v\n", len(bookings))
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			// fmt.Printf("These are all our bookings: %v\n", bookings)
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our cnference is booked out. Come back next year")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email addres you entered diesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+
+			// fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			fmt.Println("Your input value is invalid, please try again.\n")
 		}
-		// fmt.Printf("These are all our bookings: %v\n", bookings)
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 	}
 }
